da5e078d4493a03a0e71b544be9cb901f2cabf6a
Isaac Cornejo <espinosacornejo.pr@gmail.com>
2024-04-30 02:07:26 -0600

    arreglando media querys de menu mobile y funciones del meni

--- assets/scripts/menu.js
+++ assets/scripts/menu.js
@@ -7,20 +7,20 @@ const links = document.querySelectorAll(".menu__element");
 btnMenu.addEventListener("click", () => {
   nav.classList.add("nav--mobile");
   navMenu.classList.add("nav__menu--mobile");
-  btnMenu.style.display = "none";
   btnClose.classList.remove("no-mostrar");
+  btnMenu.classList.add("no-mostrar");
 });
 
 btnClose.addEventListener("click", () => {
   nav.classList.remove("nav--mobile");
   navMenu.classList.remove("nav__menu--mobile");
-  btnMenu.style.display = "block";
+  btnMenu.classList.remove("no-mostrar");
 });
 
 Array.from(links).forEach((link) => {
   link.addEventListener("click", () => {
     nav.classList.remove("nav--mobile");
     navMenu.classList.remove("nav__menu--mobile");
-    btnMenu.style.display = "block";
+    btnMenu.classList.remove("no-mostrar");
   });
 });
